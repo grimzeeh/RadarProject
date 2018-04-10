9b24397c9a1fe46c64829dd7acd8ce43569330cb
grimzeeh <38259210+grimzeeh@users.noreply.github.com>
2018-04-10 18:39:52 +0100

    Update PacketParser.kt
    test

--- src/main/kotlin/pubgradar/deserializer/PacketParser.kt
+++ src/main/kotlin/pubgradar/deserializer/PacketParser.kt
@@ -12,179 +12,206 @@ import pubgradar.struct.Bunch
 import java.util.concurrent.ConcurrentLinkedQueue
 import kotlin.concurrent.thread
 
-fun Int.d(w : Int) : String
+const val check1 = 12
+const val check2 = 8
+const val check3 = 4
+const val flag1 : Byte = 0
+const val flag2 : Byte = -1
+fun Byte.check() = this == flag1 || this == flag2
+class DevDesc(val dev : PcapNetworkInterface, val address : Inet4Address)
 {
-  return String.format("%${w}d", this)
-}
-
-val packets = ConcurrentLinkedQueue<Pair<ByteArray, Boolean>>()
-
-fun parsePackets()
-{
-  thread(isDaemon = true) {
-    while (true)
-    {
-      val work = packets.poll()
-      if (work == null)
-      {
-        Thread.yield()
-        continue
-      }
-      val (raw, client) = work
-      proc_raw_packet(raw, client)
-    }
+  override fun toString() : String
+  {
+    return "[${address.hostAddress}] - ${dev.description}"
   }
 }
 
-fun proc_raw_packet(raw : ByteArray, client : Boolean = true)
+enum class SniffOption
 {
-  if (raw.isEmpty()) return
-  var lastByte = raw.last().toInt() and 0xFF
-  if (lastByte != 0)
-  {
-    var bitsize = (raw.size * 8) - 2
-    while ((lastByte and 0x80) == 0)
-    {
-      lastByte *= 2
-      bitsize--
-    }
-    val reader = Buffer(raw, 0, bitsize)
-    reader.proc_raw_packet(client)
-  }
+  PortFilter,
+  PPTPFilter
 }
 
-fun Buffer.proc_raw_packet(client : Boolean)
+val settingHome = "${System.getProperty("user.home")}$separator.gaydar"
+
+class Sniffer
 {
-  if (readBit()) return
-  if (readBit()) return
-  val packetId = readInt(MAX_PACKETID)
-  while (notEnd())
+
+  companion object : GameListener
   {
-    val IsAck = readBit()
-    if (IsAck)
+
+    override fun onGameOver()
     {
-      val ackPacketId = readInt(MAX_PACKETID)
-      val bHasServerFrameTime = readBit()
-      val remoteInKBytesPerSecond = readIntPacked()
-      continue
-    }
-    val bControl = readBit()
-    var bOpen = false
-    var bClose = false
-    var bDormant = false
-    if (bControl)
-    {
-      bOpen = readBit()
-      bClose = readBit()
-      if (bClose) bDormant = readBit()
-    }
-    val bIsReplicationPaused = readBit()
-    val bReliable = readBit()
-    val chIndex = readInt(MAX_CHANNELS)
-    val a = readBit()
-    val bHasPackageMapExports = readBit()
-    val bHasMustBeMappedGUIDs = readBit()
-    val bPartial = readBit()
-    val chSequence = when
-    {
-      bReliable -> readInt(MAX_CHSEQUENCE)
-      bPartial  -> packetId
-      else      -> 0
-    }
-    var bPartialInitial = false
-    var bPartialFinal = false
-    if (bPartial)
-    {
-      bPartialInitial = readBit()
-      bPartialFinal = readBit()
     }
 
-    val chType = if (bReliable || bOpen) readInt(CHTYPE_MAX) else CHTYPE_NONE
-    if (chType > 4)
-      return
-    val bunchDataBits = readInt(MAX_PACKET_SIZE * 8)
-    val pre = bitsLeft()
-    if (bunchDataBits > pre)
-      return
+    private val nif : PcapNetworkInterface
+    val targetAddr : Inet4Address
+    val sniffOption : SniffOption
+    var selfCoords = Vector2()
 
-    val channels = if (client) inChannels else outChannels
-    val closedChannels = if (client) closedInChannels else closedOutChannels
-    if (chIndex !in channels && (chIndex != 0 || chType != CHTYPE_CONTROL))
-    // Can't handle other channels until control channel exists.
-      if (client && channels[0] == null)
+    init
+    {
+      register(this)
+
+      val nif : PcapNetworkInterface?
+      val sniffOption : SniffOption?
+      val devs = Pcaps.findAllDevs()
+
+      val choices = ArrayList<DevDesc>()
+      for (dev in devs)
+        dev.addresses
+              .filter { it.address is Inet4Address }
+              .mapTo(choices) { DevDesc(dev, it.address as Inet4Address) }
+
+      if (choices.isEmpty())
       {
-        return
+        System.exit(-1)
       }
 
-    // ignore control channel close if it hasn't been opened yet
-    if (chIndex == 0 && channels[0] == null && bClose && chType == CHTYPE_CONTROL)
-    {
-      return
+      val devDesc = choices.first { it.address.hostAddress == Args[0] }
+      nif = devDesc.dev
+      val localAddr = if (Args.size == 3) InetAddress.getByName(Args[2]) as Inet4Address else devDesc.address
+      sniffOption = SniffOption.valueOf(Args[1])
+      this.nif = nif
+      this.targetAddr = localAddr
+      this.sniffOption = sniffOption
+
     }
 
-    if (chIndex !in channels && !bReliable)
+    val localAddr = targetAddr
+
+    const val snapLen = 65536
+    val mode = PROMISCUOUS
+    const val timeout = 1
+
+    const val PPTPFlag : Byte = 0b0011_0000
+    const val ACKFlag : Byte = 0b1000_0000.toByte()
+
+    fun ByteArray.toIntBE(pos : Int, num : Int) : Int
     {
-      //Unreliable bunches that open channels should be bOpen && (bClose || bPartial)
-      val validUnreliableOpen = bOpen && (bClose || bPartial)
-      if (!validUnreliableOpen)
+      var value = 0
+      for (i in 0 until num)
+        value = value or ((this[pos + num - 1 - i].toInt() and 0xff) shl 8 * i)
+      return value
+    }
+
+    fun parsePPTPGRE(raw : ByteArray) : Packet?
+    {
+      var i = 0
+      if (raw[i] != PPTPFlag) return null//PPTP
+      i++
+      val hasAck = (raw[i] and ACKFlag) != 0.toByte()
+      i++
+      val protocolType = raw.toIntBE(i, 2)
+      i += 2
+      if (protocolType != 0x880b) return null
+      val payloadLength = raw.toIntBE(i, 2)
+      i += 2
+      val callID = raw.toIntBE(i, 2)
+      i += 2
+      val seq = raw.toIntBE(i, 4)
+      i += 4
+      if (hasAck)
       {
-        skipBits(bunchDataBits)//此bunch不处理
-        continue
+        val ack = raw.toIntBE(i, 4)
+        i += 4
       }
+      if (raw[i] != 0x21.toByte()) return null//not ipv4
+      i--
+      raw[i] = 0
+      val pppPkt = PppSelector.newPacket(raw, i, raw.size - i)
+      return pppPkt.payload
     }
 
-    if (chIndex !in channels)
+    fun udp_payload(packet : Packet) : UdpPacket?
     {
-      when (chType)
+      return when (sniffOption)
       {
-        CHTYPE_CONTROL            ->
-        {
-          channels[chIndex] = ControlChannel(chIndex, client)
+        PortFilter -> packet
+        PPTPFilter -> parsePPTPGRE(packet[IpV4Packet::class.java].payload.rawData)
+
+      }?.get(UdpPacket::class.java)
+    }
+
+    fun sniffLocationOnline()
+    {
+      val handle = nif.openLive(snapLen, mode, timeout)
+      val filter = when (sniffOption)
+      {
+        PortFilter -> "udp portrange 7000-7999"
+        PPTPFilter -> "ip[9]=47"
+      }
+      handle.setFilter(filter, OPTIMIZE)
+      thread(isDaemon = true) {
+        handle.loop(-1) { packet : Packet? ->
+          try
+          {
+            packet!!
+            val ip = packet[IpPacket::class.java]
+            val udp = udp_payload(packet) ?: return@loop
+            val raw = udp.payload.rawData
+
+            if (udp.header.dstPort.valueAsInt() in 7000..7999)
+              packets.add(Pair(raw, false))
+            else if (udp.header.srcPort.valueAsInt() in 7000..7999)
+              packets.add(Pair(raw, true))
+          }
+          catch (e : Exception)
+          {
+          }
         }
-        CHTYPE_VOICE, CHTYPE_FILE ->
-        {
-
-        }
-        else                      ->
-        {
-          if (chType == CHTYPE_NONE)
-            println("$chSequence lost the first actor creation bunch. just create as we need it.")
-          inChannels[chIndex] = ActorChannel(chIndex, true)
-          outChannels[chIndex] = ActorChannel(chIndex, false)
-        }
       }
+      parsePackets()
     }
-    val chan = channels[chIndex]
 
-    if (chan != null)
+
+    fun sniffLocationOffline(filename : String? = "c:\\TestServer01.pcap")
     {
-      check(chType == CHTYPE_NONE || chType == chan.chType)
-      try
-      {
-        val bunch = Bunch(
-              bunchDataBits,
-              deepCopy(bunchDataBits),
-              packetId,
-              chIndex,
-              chType,
-              chSequence,
-              bOpen,
-              bClose,
-              bDormant,
-              bIsReplicationPaused,
-              bReliable,
-              bPartial,
-              bPartialInitial,
-              bPartialFinal,
-              bHasPackageMapExports, bHasMustBeMappedGUIDs
-                         )
-        chan.ReceivedRawBunch(bunch)
+      thread(isDaemon = true) {
+        //                val files = arrayOf("d:\\test10.pcap", "d:\\test11.pcap", "d:\\test12.pcap")
+//        val files = arrayOf("d:\\pptp02.pcap")
+        val files = arrayOf(filename)
+        for (file in files)
+        {
+          val handle = Pcaps.openOffline(file)
+
+          while (true)
+          {
+            try
+            {
+              val packet = handle.nextPacket ?: break
+              val ip = packet[IpPacket::class.java]
+              val udp = Sniffer.udp_payload(packet) ?: continue
+              val raw = udp.payload.rawData
+
+              if (udp.header.dstPort.valueAsInt() in 7000..7999)
+                packets.add(Pair(raw, false))
+              else if (udp.header.srcPort.valueAsInt() in 7000..7999)
+                packets.add(Pair(raw, true))
+
+            }
+            catch (e : Exception)
+            {
+            }
+            catch (e : IndexOutOfBoundsException)
+            {
+            }
+            catch (e : Exception)
+            {
+            }
+            catch (e : NotOpenException)
+            {
+              e.printStackTrace()
+            }
+            Thread.sleep(2)
+
+          }
+        }
       }
-      catch (e : Exception)
-      {
-      }
+      parsePackets()
     }
-    skipBits(bunchDataBits)
+  }
+}
   }
   return
 }
